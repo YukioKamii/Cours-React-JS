a5f0126532ca21b24f233f4e9bd55a35e5a1cd03
YukioKamii <yoyolc.2004@gmail.com>
2024-05-06 11:48:11 +0200

    add button n card to courses list

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,9 +1,11 @@
+import Card from '../components/CoursesIndex/Card';
 import './Courses.css';
 
 function Courses(){
     return(
         <>
           <h1>Liste de cours</h1>
+          <Card title="Niglot de ses morts" content="adzucibciubzicbiuzbiucibzzzc"/>
         </>
     );
 }
