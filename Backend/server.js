cb773e0a1210abcb3a06c21909b1f3ba0ff20496
YukioKamii <yoyolc.2004@gmail.com>
2024-05-28 15:38:54 +0200

    create server js

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000; // Use the port provided by the host or default to 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
